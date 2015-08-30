437fb47e937ef7366784eaec6b1e25056209c401
JohnnyPestana <johnny_gabriel@outlook.com>
2015-08-30 13:07:19 -0300

    Commit Inicial

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	less = require('gulp-less'),
+	path = require('path'),
+	rename = require('gulp-rename'),
+	minifyCSS = require('gulp-minify-css'),
+	watch = require('gulp-watch'),
+	useref = require('gulp-useref'),
+	server = require('gulp-server-livereload');
+
+gulp.task('less', function() {
+	return gulp.src('source/less/**/*.less')
+		.pipe(less({
+			paths: [ path.join(__dirname, 'less') ]
+		}))
+		.pipe(gulp.dest('source/css'))
+		.pipe(minifyCSS())
+		.pipe(rename({suffix: '-min'}))
+		.pipe(gulp.dest('source/css'));
+});
+
+gulp.task('watch', function() {
+	watch(['source/less/**/*.less', 'source/**/*.html'], function() {
+		gulp.start('less', 'bundle');
+	})
+});
+
+gulp.task('bundle', function() {
+	var assets = useref.assets();
+	return gulp.src('source/index.html')
+		.pipe(assets)
+		.pipe(assets.restore())
+		.pipe(useref())
+		.pipe(gulp.dest('public'));
+});
+
+gulp.task('server', function() {
+	gulp.src('public')
+		.pipe(server({
+			livereload: true,
+			open: true,
+			defaultFile: 'index.html'
+		}))
+});
